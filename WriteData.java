73fab3c7e5fada395bdd923a201bb68d278163bd
yiy1156 <87553141+yiy1156@users.noreply.github.com>
2021-07-16 22:57:52 -0400

    Add files via upload

---
+++ WriteData.java
@@ -0,0 +1,27 @@
+package Lec17;
+
+public class WriteData {
+  public static void main(String[] args) throws Exception {
+    java.io.File file = new java.io.File("scores.txt");
+    if (file.exists()) {
+      System.out.println("File already exists");
+    // System.exit(0);
+    }
+
+    // Create a file
+    java.io.PrintWriter output = new java.io.PrintWriter(file);
+ char[] arr = {'a','b','y','z'};
+
+    // Write formatted output to the file
+    output.print("John T Smith ");
+    output.println(90);
+    output.print("Eric K Jones ");
+    output.println(85);
+    output.print(arr);
+    
+    
+
+    // Close the file
+    output.close();
+  }
+}
